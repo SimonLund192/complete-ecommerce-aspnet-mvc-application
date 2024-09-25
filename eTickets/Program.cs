9cc319b958ebc91b6127a5b3d9861d8a530a6409
SimonLund192 <10503079@ucn.dk>
2024-09-25 13:04:59 +0200

    Rettelser til video 27

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -21,8 +21,6 @@ namespace eTickets
 
             var app = builder.Build();
 
-
-
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
@@ -30,9 +28,6 @@ namespace eTickets
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            
-
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
